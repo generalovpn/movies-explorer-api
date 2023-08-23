2bdde17f8f8db2b28c98c9da74ba404939dc485f
Pavel Generalov <generalovpn@yandex.ru>
2023-08-23 07:46:33 +0300

    fix: Настроить cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connect(DB_URL);
 app.use(helmet());
 app.use(requestLogger);
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://movies.generalovpn.nomoreparties.sbs'],
+  origin: ['http://localhost:3000', 'http://api.movies.generalovpn.nomoreparties.sbs'],
   credentials: true,
 }));
 app.options('/', cors());
